58eebdf3f7b271c04d0a781f704a9ab8793c1eee
Felipe <felipegamae2@gmail.com>
2026-05-07 15:41:41 -0300

    Put the CRUD headers an methods

--- API/main.py
+++ API/main.py
@@ -26,8 +26,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://npecfainor.vercel.app"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="admin/login")
